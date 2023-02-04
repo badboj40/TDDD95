559ff407f151610d6e1d9a7ecd24b91d40a666e9
Gustav <gusel725@student.liu.se>
2023-02-04 23:48:43 +0100

    1 byte save on chopwood

--- golf/chopwood.py
+++ golf/chopwood.py
@@ -6,7 +6,7 @@ for v in a:d[v-1]+=1
 [heappush(q,i)for i in range(n)if d[i]<2]
 for v in a:
  if q:
-  u=heappop(q);r+=[u+1];d[u]-=1;d[v-1]-=1
+  u=heappop(q);r+=[u+1];d[u]=0;d[v-1]-=1
   if d[v-1]==1:heappush(q,v-1)
 if sum(d)>1:r=['Error']
 print(*r)
