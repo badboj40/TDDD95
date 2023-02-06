903a06091089187568bd46c4bfe194d80c32f0a1
Gustav <gusel725@student.liu.se>
2023-02-06 23:29:29 +0100

    shortestpath1 golf improvement

--- golf/shortestpath1.py
+++ golf/shortestpath1.py
@@ -1,6 +1,6 @@
 from heapq import*;z=float('inf')
 f=lambda x:[[*map(int,input().split())]for _ in[1]*x]
-while sum(i:=[*map(int,input().split())]):
+while sum(i:=f(1)[0]):
  n,m,q,s=i;g=[[]for _ in[1]*n];d={};Q=[(0,s)]
  for u,v,w in f(m):g[u]+=[(w,v)]
  while Q:
