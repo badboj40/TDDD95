d54fc84dbb09011e46f855d6e3aa1b9abaec1645
Gustav <gusel725@student.liu.se>
2023-02-07 14:33:28 +0100

    allpairspath improvement

--- golf/allpairspath.py
+++ golf/allpairspath.py
@@ -1,14 +1,14 @@
-z=float('inf');r=range
-f=lambda x:[[*map(int,input().split())]for _ in r(x)]
+z=float('inf')
+f=lambda x:[[*map(int,input().split())]for _ in[1]*x]
 while sum(g:=f(1)[0]):
- n,m,q=g;e=f(m);y=f(q);d=[n*[z]for _ in r(n)]
- for u,v,w in e:d[u][v]=min(d[u][v],w)
- for v in r(n):d[v][v]=0
- for k in r(n):
-  for i in r(n):
-   for j in r(n):d[i][j]=min(d[i][j],d[i][k]+d[k][j])
- for i in r(n):
-  for j in r(n):
-   for k in r(n):
+ n,m,q=g;r=range(n);d=[n*[z]for _ in r]
+ for u,v,w in f(m):d[u][v]=min(d[u][v],w)
+ for v in r:d[v][v]=0
+ for k in r:
+  for i in r:
+   for j in r:d[i][j]=min(d[i][j],d[i][k]+d[k][j])
+ for i in r:
+  for j in r:
+   for k in r:
     if d[k][k]<0 and z>d[i][k]and d[k][j]<z:d[i][j]=-z
- print(*['Impossible'*(d[u][v]==z)or'-Infinity'*(d[u][v]==-z)or d[u][v]for u,v in y])
+ print(*['Impossible'*(d[u][v]==z)or'-Infinity'*(d[u][v]==-z)or d[u][v]for u,v in f(q)])
