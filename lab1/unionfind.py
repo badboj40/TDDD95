e4317322801c1f4c6083a6cd2ddb531a7d8fc34d
Gustav <gusel725@student.liu.se>
2023-02-19 15:51:56 +0100

    Union Find improvement

--- lab1/unionfind.py
+++ lab1/unionfind.py
@@ -3,52 +3,47 @@ import sys
 __author__ = "Gustav Elmqvist"
 
 """
-	An implementation of a data structure to solve the Union-Find problem.
-	From the start there are N different sets, but they are later joined
-	together with the union function. Because the find function squashes
-	the parent relationship graph with each call the time complexity is
-	logarithmic.
+    An implementation of a data structure to solve the Union-Find problem.
+    From the start there are N different sets, but they are later joined
+    together with the union function. Because the find function squashes
+    the parent relationship graph with each call the time complexity is
+    logarithmic.
 
 """
 
 class DSU:
-	"""
-	Disjoint Set Union data structure implementation.
-	"""
+    """
+    Disjoint Set Union data structure implementation.
+    """
 
-	def __init__(self, N: int) -> None:
-		self.parent = [n for n in range(N)]
+    def __init__(self, N: int) -> None:
+        self.parent = [n for n in range(N)]
 
-	def find(self, v: int) -> int:
-		"""
-		Find the parent of the set that v is in.
+    def find(self, v: int) -> int:
+        """
+        Find the parent of the set that v is in.
 
-		Time complexity: O(logn)
-		"""
-		if v == self.parent[v]:
-			return v
-		self.parent[v] = self.find(self.parent[v])
-		return self.parent[v]
+        Time complexity: O(logn)
+        """
+        if v != self.parent[v]:
+            self.parent[v] = self.find(self.parent[v]) 
+        return self.parent[v]
 
-	def union(self, a: int, b: int) -> None:
-		"""
-		Merge the sets containing the elements a and b.
+    def union(self, a: int, b: int) -> None:
+        """
+        Merge the sets containing the elements a and b.
 
-		Time complexity: O(logn)
-		"""
-		self.parent[self.find(b)] = self.find(a)
-		a = self.find(a)
-		b = self.find(b)
-		if a != b:
-			self.parent[b] = a
+        Time complexity: O(logn)
+        """
+        self.parent[self.find(b)] = self.find(a)
 
-	def same(self, a: int, b: int) -> bool:
-		"""
-		Test whether a and b are in the same set.
+    def same(self, a: int, b: int) -> bool:
+        """
+        Test whether a and b are in the same set.
 
-		Time complexity: O(logn)
-		"""
-		return self.find(a) == self.find(b)
+        Time complexity: O(logn)
+        """
+        return self.find(a) == self.find(b)
 
 
 if __name__ == "__main__":
